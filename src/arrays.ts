0a2637a452d01e5fe7336209511911852e53cf2e
Gabriel Simpson <gabe@gabrielsimpson.com>
2024-02-20 22:00:13 -0500

    stringsToIntegers

--- src/arrays.ts
+++ src/arrays.ts
@@ -22,7 +22,10 @@ export function tripleNumbers(numbers: number[]): number[] {
  * the number cannot be parsed as an integer, convert it to 0 instead.
  */
 export function stringsToIntegers(numbers: string[]): number[] {
-    return [];
+    const converted = numbers.map((number: string): number =>
+        !Number.isNaN(+number) ? +number : 0
+    );
+    return converted;
 }
 
 /**
